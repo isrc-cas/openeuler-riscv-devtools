47b59b8a603197ecea1095faa6596de289b3b126
Wei Wu <wuwei2016@iscas.ac.cn>
2021-11-24 23:28:43 +0800

    vm_config.sh: Update yum urls

--- vm_config.sh
+++ vm_config.sh
@@ -2,12 +2,29 @@
 
 # Need root
 
+# ref: https://gitee.com/zxs-un/openEuler-port2riscv64/blob/master/doc/vm-qemu-openEuler-riscv64.md
+
 # Trick: get current time directly from NTP servers.
 
 cat </dev/tcp/time.nist.gov/13 | awk '{print $2, $3}' | xargs -I {} date -s "{}"
 
 systemctl disable auditd
 systemctl disable systemd-networkd-wait-online
+
+mv /etc/yum.repos.d/oe-rv.repo{,.orig}
+cat > /etc/yum.repos.d/oe-rv.repo << "EOT"
+[base]
+name=base
+baseurl=https://isrc.iscas.ac.cn/mirror/openeuler-sig-riscv/oe-RISCV-repo/
+enabled=1
+gpgcheck=0
+
+[everything]
+name=everything
+baseurl=https://repo.openeuler.org/openEuler-preview/RISC-V/everything/
+enabled=1
+gpgcheck=0
+EOT
 
 yum update -y
 yum install -y osc sudo
